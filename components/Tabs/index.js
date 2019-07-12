263e6e5bbbd62a304ca1c8d43ebbb1b05ec232a0
HannahMarieWieser <nikauli@gmail.com>
2019-07-12 13:26:14 -0400

    debugging node problem

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,46 @@
 //
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
+
+
+const addtab = document.querySelector('.topics')
+
+const tabdata = [axios.get('https://lambda-times-backend.herokuapp.com/topics')]
+console.log(tabdata)
+
+tabdata.forEach(element => {
+    console.log('element', element)
+
+    element
+    .then(data => {
+        console.log('sucess', data)
+
+
+        const axiodata = data.data;
+        const topicarray = axiodata.topics
+        console.log('topicarray:', topicarray)
+
+        topicarray.forEach(tabitem =>{
+            console.log('name', tabitem)
+
+            const newtabfromarr = createTabs(tabitem)
+            addtab.appendChild(newtabfromarr)
+        })
+
+        
+        
+
+    })
+});
+
+
+
+function createTabs(tabdata){
+    const tabdiv = document.createElement('div');
+
+    tabdiv.classList.add('tab')
+
+    tabdiv.textContent = tabdata.topic
+
+    return createTabs
+}
